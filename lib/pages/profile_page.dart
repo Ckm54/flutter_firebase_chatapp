b2332b1f9c5cefaafdb3abf23b76c431dc4999a9
collo <mutaic84@gmail.com>
2022-12-30 17:19:01 +0300

    :hammer: page update

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,17 +1,114 @@
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/pages/home_page.dart';
+import 'package:chatapp_firebase/service/auth_service.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({
+    Key? key,
+    required this.email,
+    required this.userName,
+  }) : super(key: key);
+
+  final String userName;
+  final String email;
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Profile page")),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        title: const Text("Profile",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 27,
+                fontWeight: FontWeight.bold)),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            Icon(Icons.account_circle, size: 150, color: Colors.grey[700]),
+            const SizedBox(height: 15),
+            Text(
+              widget.userName,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            const Divider(height: 2),
+            ListTile(
+              onTap: () {
+                nextScreen(context, const HomePage());
+              },
+              selectedColor: Theme.of(context).primaryColor,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.group),
+              title:
+                  const Text('Groups', style: TextStyle(color: Colors.black)),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              selectedColor: Theme.of(context).primaryColor,
+              selected: true,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.group),
+              title:
+                  const Text('Profile', style: TextStyle(color: Colors.black)),
+            ),
+            ListTile(
+              onTap: () async {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text("Logout"),
+                        content: const Text("Are you sure you want to logout?"),
+                        actions: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon:
+                                  const Icon(Icons.cancel, color: Colors.red)),
+                          IconButton(
+                              onPressed: () {
+                                authService.signOut();
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoginPage()),
+                                    (route) => false);
+                              },
+                              icon: const Icon(Icons.done, color: Colors.green))
+                        ],
+                      );
+                    });
+              },
+              selectedColor: Theme.of(context).primaryColor,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              leading: const Icon(Icons.exit_to_app),
+              title:
+                  const Text('Logout', style: TextStyle(color: Colors.black)),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
