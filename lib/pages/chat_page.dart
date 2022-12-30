c8eb43be4b661f2bb53d8c5007158d7b648d926a
collo <mutaic84@gmail.com>
2022-12-31 00:46:49 +0300

    :hammer: update scripts

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,17 +1,54 @@
+import 'package:chatapp_firebase/pages/group_info.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  const ChatPage({
+    Key? key,
+    required this.groupId,
+    required this.groupName,
+    required this.userName,
+  }) : super(key: key);
+
+  final String groupId;
+  final String groupName;
+  final String userName;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+  String admin = "";
+
+  @override
+  void initState() {
+    getChatAndAdmin();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Text(widget.groupName),
+        backgroundColor: Theme.of(context).primaryColor,
+        actions: [
+          IconButton(
+              onPressed: () {
+                nextScreen(
+                    context,
+                    const GroupInfo(
+                      groupId: widget.groupId,
+                      groupName: widget.groupName,
+                      adminName: admin,
+                    ));
+              },
+              icon: const Icon(Icons.info))
+        ],
+      ),
     );
   }
 }
