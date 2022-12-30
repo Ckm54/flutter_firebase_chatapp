a6b24cd7a1cd62fe4cf3a2aceb10a4dcd35f0d33
collo <mutaic84@gmail.com>
2022-12-30 15:30:22 +0300

    :sparkles: :lipstick: update user registration

--- lib/helper/helper_functions.dart
+++ lib/helper/helper_functions.dart
@@ -7,6 +7,21 @@ class HelperFunctions {
   static String userEmailKey = 'USEREMAILKEY';
 
   // saving data to shared preferences
+  static Future<bool> saveUserLoggedInStatus(bool isUserLoggedIn) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setBool(userLoggedInKey, isUserLoggedIn);
+  }
+
+  static Future<bool> saveUserNameSF(String userName) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userNameKey, userName);
+  }
+
+  static Future<bool> saveUserEmailSF(String userEmail) async {
+    SharedPreferences sf = await SharedPreferences.getInstance();
+    return await sf.setString(userEmailKey, userEmail);
+  }
+
   // Getting data from shared preferences
   static Future<bool?> getUserLoggedInStatus() async {
     SharedPreferences sf = await SharedPreferences.getInstance();
---
+++ lib/service/auth_service.dart
@@ -0,0 +1,38 @@
+import 'package:chatapp_firebase/helper/helper_functions.dart';
+import 'package:chatapp_firebase/service/database_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // login
+
+  // register
+  Future registerUserWithEmailAndPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+      if (user != null) {
+        // call the database service to update the user
+        DatabaseService(uid: user.uid).updateUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // signout
+  Future signOut() async {
+    try {
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
+}
