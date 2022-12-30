a82a832cbf28e56b4511feb5cee4ec7a8fcf0c6e
collo <mutaic84@gmail.com>
2022-12-30 11:53:37 +0300

    :tada: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:chatapp_firebase/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  // initialize all widgets
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // run initialization for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  } else {
+    // run initialization for android and ios
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:238171157648:web:1d5c3b5e848d8f67529415";
+  static String apiKey = "AIzaSyDaYm4Ne5QsidQToEuPCneZjgI7EOcY_1s";
+  static String messagingSenderId = "238171157648";
+  static String projectId = "chatappflutter-e0a32";
+}
